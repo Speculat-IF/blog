edd42475b83f317a23b56a0c3b90ad22a90f5aae
PL Sans <pierre-louis.sans@edu.devinci.fr>
2024-01-10 16:23:36 +0100

    :ambulance: displaying the articles and theire contents, :egg: secure easter egg, :boom: add the filter systeme, :coffin: remouved old code, :see_no_evil: add git ignore, :building_construction: rearanged image folder

--- js/main.js
+++ js/main.js
@@ -1,80 +1,54 @@
-/* menu deroulant */
-const downbtn = document.getElementById("down");
-const header = document.querySelector("header");
-const section1 = header.querySelector("section:nth-child(1)");
-const section1Nav = section1.querySelector("nav");
-downbtn.addEventListener("click", () => {
-    if (header.classList.contains("collapsed")) {
-        header.classList.remove("collapsed");
-        header.classList.add("expanded");
-        section1.style.display = "flex";
-        section1.style.flexDirection = "column";
-        section1.style.gap = "10%";
-        section1Nav.style.display = "flex";
-        section1Nav.style.flexDirection = "column";
-        section1Nav.style.gap = "20px";
-        downbtn.style.transform = "rotate(180deg)";
-    } else {
-        header.classList.remove("expanded");
-        header.classList.add("collapsed");
-        section1.style.flexDirection = "row";
-        section1.style.gap = "25%";
-        section1Nav.style.display = "none";
-        section1Nav.style.flexDirection = "row";
-        section1Nav.style.gap = "0";
-        downbtn.style.transform = "rotate(0deg)";
-    }
-});
-
-/* Array des tags */
+/* Global */
 const tagcolor = {
     "Biologique": "green",
-    "IA": "grey",
+    "IA": "red",
     "Quantique": "orange",
     "Espace": "blue",
     "Social": "pink"
 };
 
+const auteur = ['Ethan', 'Arthur', 'Ethra', 'Lucee', 'Ali', 'Pierre-Louis', 'Dragibo']
 
-/* Afficher les articles sur la page d'accueil */
+const url ="https://raw.githubusercontent.com/Speculat-IF/blog/main/biblioteque.json"
+
+const urlImage = "https://github.com/Speculat-IF/blog/blob/main/imgArticle/"
+
+
+/* Js Page Accueil */
 if (window.location.href.includes("index.html") || window.location.href.includes("https://speculat-if.github.io/blog/")) {
-    const url ="https://raw.githubusercontent.com/Speculat-IF/blog/main/biblioteque.json"
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-        // console.log(data);
-        let dataLength = data.articles.length;
-        for (let i = Math.max(0, dataLength - 4); i < dataLength; i++) {
-            const cards = document.getElementById("cards");
-            let card = document.createElement("div");
-            card.classList.add("card");
-            let titre = document.createElement("h3");
-            let resume = document.createElement("p");
-            let alltags = document.createElement("div");
-            titre.innerHTML = data.articles[i].titre;
-            var imagePath = 'img/Barghest.png'; var img = new Image(); img.onload = () => console.log('Good is working'); 
-            img.onerror = () => window.href.location = "https://developer.mozilla.org/fr/docs/Web/HTTP/Status/418"; img.src = imagePath;
-            resume.innerHTML = data.articles[i].contenu.split(" ").slice(0,20).join(" ") + "..." + "<br><br>" + data.articles[i].date;
-            let tagsLength = data.articles[i].tags.length;
-            for (let j = 0; j < tagsLength; j++) {
-                let tag = document.createElement("div");
-                tag.classList.add("tag");
-                let tagItem = document.createElement("span");
-                tagItem.innerHTML = data.articles[i].tags[j];
-                tag.appendChild(tagItem);
-                tag.classList.add(tagcolor[data.articles[i].tags[j]]);
-                alltags.appendChild(tag);
+        reverse = data.articles.reverse();
+        const posts = document.querySelectorAll(".post");
+        var imagePath = 'images/Barghest.png'; 
+        var img = new Image(); img.onload = () => console.log('No error detected'); 
+        for (let i = 0; i < 3; i++) {
+            let post = posts[i];
+            let postTitle = post.querySelector(".titre");
+            postTitle.innerHTML = reverse[i].titre;
+            let postDate = post.querySelector(".date");
+            postDate.innerHTML = reverse[i].date;
+            let postTags = post.querySelector(".tags");
+            let tagslist = reverse[i].tags;
+            for (let j = 0; j < tagslist.length; j++) {
+                let tag = document.createElement("span");
+                tag.innerHTML = tagslist[j] + "  ";
+                tag.style.color = tagcolor[tagslist[j]];
+                postTags.appendChild(tag);
             }
-            alltags.style.display = "flex";
-            alltags.style.gap = "10px";
-            card.appendChild(alltags);
-            card.appendChild(titre);
-            card.appendChild(resume);
-            cards.appendChild(card);
-            card.addEventListener("click", () => {
-                window.location.href = "article.html?id=" + data.articles[i].id;
+            let postPrevue = post.querySelector(".prevue");
+            if (postPrevue != null) {
+                postPrevue.innerHTML = reverse[i].contenu.split(" ").slice(0,50).join(" ") + "...";
+            }
+            let postTumbnail = post.querySelector(".tumbnail");
+            img.onerror = () => window.href.location = "https://developer.mozilla.org/fr/docs/Web/HTTP/Status/418"; img.src = imagePath;
+            postTumbnail.src = urlImage + reverse[i].tumbnail;
+            post.addEventListener("click", () => {
+                window.location.href = "article.html?id=" + reverse[i].id;
             });
         }
+        
     })
     .catch((error) => {
         console.error('Error fetching data:', error);
@@ -83,11 +57,134 @@ if (window.location.href.includes("index.html") || window.location.href.includes
 
 }
 
+/* Js Page Articles */
+if (window.location.href.includes("articles.html")) {
+    const filters = document.querySelector(".filters");
 
-/* Afficher un article cliquer */
+    for (let i = 0; i < Object.keys(tagcolor).length; i++) {
+        let button = document.createElement("button");
+        button.innerHTML = Object.keys(tagcolor)[i];
+        button.classList.add("filter");
+        filters.appendChild(button)
+    }
 
+    auteur.forEach((auteur) => {
+        let button = document.createElement("button");
+        button.innerHTML = auteur;
+        button.classList.add("filter");
+        filters.appendChild(button)
+    });
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((allArticle) => {
+            let dataLength = allArticle.articles.length;
+            let posts = document.querySelector(".posts");
+            for (let i = 1; i < dataLength; i++) {
+                let curentArticle = allArticle.articles[i];
+                let post = document.createElement("div");   
+                post.classList.add("post");
+                var imagePath = 'images/Barghest.png'; 
+                var img = new Image(); img.onload = () => console.log('No error detected');
+                let tumbnail = document.createElement("img");
+                tumbnail.src = urlImage + curentArticle.tumbnail;
+                tumbnail.alt = 'tubnail de l\'article';
+                let info = document.createElement("div");
+                info.classList.add("info");
+                let data = document.createElement("div");
+                let titre = document.createElement("h3");
+                titre.innerHTML = curentArticle.titre;
+                let date = document.createElement("p");
+                date.innerHTML = curentArticle.date;
+                let auteur = document.createElement("p");
+                auteur.innerHTML = curentArticle.auteur;
+                let tags = document.createElement("p");
+                let tagslist = curentArticle.tags;
+                for (let j = 0; j < tagslist.length; j++) {
+                    let tag = document.createElement("span");
+                    tag.innerHTML = tagslist[j] + "  ";
+                    tag.style.color = tagcolor[tagslist[j]];
+                    tags.appendChild(tag);
+                }
+                auteur.style.color = "#2F4DDB";
+                auteur.classList.add("auteur");
+                data.appendChild(auteur);
+                tags.style.display = "none";
+                tags.classList.add("tags");
+                data.appendChild(tags);
+                data.appendChild(date);
+                data.style.display = "flex";
+                data.style.flexDirection = "row";
+                data.style.justifyContent = "space-between";
+                img.onerror = () => 
+                window.href.location = "https://developer.mozilla.org/fr/docs/Web/HTTP/Status/418"; 
+                img.src = imagePath;
+                info.appendChild(data);
+                info.appendChild(titre);
+                post.appendChild(tumbnail);
+                post.appendChild(info);
+                post.addEventListener("click", () => {
+                    console.log(curentArticle.id);
+                    window.location.href = "article.html?id=" + curentArticle.id;
+                });
+                posts.appendChild(post);
+            }
+        })
+
+
+        let filtersList = document.querySelectorAll(".filter");
+        filtersList.forEach((filter) => {
+            filter.addEventListener("click", () => {
+                filtersList.forEach((filter) => {
+                    filter.classList.remove("active");
+                });
+                filter.classList.add("active");
+                let posts = document.querySelectorAll(".post");
+                if (filter.innerHTML != "Tous") {
+                    posts.forEach((post) => {
+                        post.style.display = "none";
+                        let tags = post.querySelector(".tags");
+                        if (tags != null) {
+                            tags = tags.querySelectorAll("span");
+                            let tagsLength = tags.length;
+                            let tag1 = null;
+                            let tag2 = null;
+                            if (tagsLength = 1) {
+                                tag1 = tags[0];
+                            } else if (tagsLength = 2) {
+                                tag1 = tags[0];
+                                tag2 = tags[1];
+                            }
+                            if (tag1 != null) {
+                                if (tag1.innerHTML.includes(filter.innerHTML)) {
+                                    post.style.display = "flex";
+                                }
+                            }
+                            if (tag2 != null) {
+                                if (tag1.innerHTML.includes(filter.innerHTML) || tag2.innerHTML.includes(filter.innerHTML)) {
+                                    post.style.display = "flex";
+                                }
+                            }
+                        }
+                        let auteur = post.querySelector(".auteur");
+                        if (auteur != null) {
+                            if (auteur.innerHTML.includes(filter.innerHTML)) {
+                                post.style.display = "flex";
+                            }
+                        }
+                    });
+                } else {
+                    posts.forEach((post) => {
+                        post.style.display = "flex";
+                    });
+                }
+            });
+        });
+    
+}
+
+/* js Page Article */
 if (window.location.href.includes("article.html")) {
-    const url ="https://raw.githubusercontent.com/Speculat-IF/blog/main/biblioteque.json"
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
@@ -96,51 +193,26 @@ if (window.location.href.includes("article.html")) {
         let id = searchParams.get("id");
         let searchedArticle = data.articles.find((article) => article.id == id);
         console.log(searchedArticle);
-        let firstdiv = document.createElement("div");
-        let seconddiv = document.createElement("div");
-        let thirddiv = document.createElement("div");
-        let fourthdiv = document.createElement("div");
-
-        let titre = document.createElement("h1");
+        let titre = document.querySelector(".titre");
         titre.innerHTML = searchedArticle.titre;
-        let underTitre = document.createElement("h2");
-        underTitre.innerHTML = searchedArticle.date + " - " + searchedArticle.auteur;
-        firstdiv.appendChild(titre);
-        firstdiv.appendChild(underTitre);
-
-        let halftext = document.createElement("p");
-        halftext.innerHTML = searchedArticle.contenu.split(" ").slice(0,searchedArticle.contenu.split(" ").length/2).join(" ");
-        let firstillustration = document.createElement("img");
-        firstillustration.src = searchedArticle.illustration;
-        seconddiv.appendChild(halftext);
-        seconddiv.appendChild(firstillustration);
-
-        let secondhalftext = document.createElement("p");
-        secondhalftext.innerHTML = searchedArticle.contenu.split(" ").slice(searchedArticle.contenu.split(" ").length/2,searchedArticle.contenu.split(" ").length).join(" ");
-        let secondillustration = document.createElement("img");
-        secondillustration.src = searchedArticle.illustration;
-        thirddiv.appendChild(secondillustration);
-        thirddiv.appendChild(secondhalftext);
-        
-        let h2 = document.createElement("h2");
-        h2.innerHTML = "Sources :";
-        let ul = document.createElement("ul");
-        let sourcesLength = searchedArticle.source.length;
-        for (let i = 0; i < sourcesLength; i++) {
-            let li = document.createElement("li");
-            let a = document.createElement("a");
-            a.innerHTML = searchedArticle.source[i].lien;
-            li.appendChild(a);
-            ul.appendChild(li);
-        }
-        fourthdiv.appendChild(h2);
-        fourthdiv.appendChild(ul);
-
-        let articlepage = document.getElementById("articlepage");
-        articlepage.appendChild(firstdiv);
-        articlepage.appendChild(seconddiv);
-        articlepage.appendChild(thirddiv);
-        articlepage.appendChild(fourthdiv);
+        let date = document.querySelector(".date");
+        date.innerHTML = searchedArticle.date;
+        let auteur = document.querySelector(".auteur");
+        auteur.innerHTML = searchedArticle.auteur;
+        let textP1 = document.querySelector(".textP1");
+        textP1.innerHTML = searchedArticle.contenu.split(" ").slice(0,searchedArticle.contenu.split(" ").length/2).join(" ") + "...";
+        let textP2 = document.querySelector(".textP2");
+        textP2.innerHTML = "..." + searchedArticle.contenu.split(" ").slice(searchedArticle.contenu.split(" ").length/2,searchedArticle.contenu.split(" ").length).join(" ");
+        let srcs = document.querySelector(".srcs");
+        searchedArticle.source.forEach((source) => {
+            let p = document.createElement("p");
+            let span = document.createElement("span");
+            span.innerHTML = source.nom;
+            p.appendChild(span);
+            p.innerHTML += " : " + source.lien;
+            srcs.appendChild(p);
+        });
+       
     })
     .catch((error) => {
         console.error('Error fetching data:', error);
