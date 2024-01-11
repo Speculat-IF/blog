d6669e67eb969943b3f9659fd9ab991697c66e6c
Nalithdev <Ethan.delcroix@outlook.fr>
2024-01-11 11:15:39 +0100

    create persistance on dark mode

--- js/main.js
+++ js/main.js
@@ -278,6 +278,7 @@ if (window.location.href.includes("article.html")) {
 }
 
 let favtag= true;
+let darktag= false;
 function dark_mode(){
   const body = document.querySelector("body");
   const header = document.querySelector("header");
@@ -321,5 +322,13 @@ function dark_mode(){
   label.forEach(element => {
     element.classList.toggle("t-darkmode");
   });
+  if (darktag === false) {
+    localStorage.setItem("darkmode", "true");
+    darktag = true;
+  }
 
+}
+
+if (localStorage.getItem("darkmode") === "true") {
+  dark_mode();
 }
